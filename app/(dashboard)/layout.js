3c9075a26876d6aa3d51265d94535a6ff117688e
Ahmad Munab <ahmadmunab22@gmail.com>
2023-11-10 17:58:13 +0600

    Toastify bug fix

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,38 +1,36 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
-import "react-toastify/dist/ReactToastify.css";
 
 import React from "react";
-import { ToastContainer } from "react-toastify";
 
 const DashboardLayout = ({ children }) => {
   return <h1 className="text-5xl font-bold">WORK IN PROGRESS</h1>;
 
-  return (
-    <div className="h-full relative p-0">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        <Sidebar />
-      </div>
-      <main className="md:pl-72 flex flex-col h-full">
-        <Navbar />
-        {children}
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-        {/* Same as */}
-        <ToastContainer />
-      </main>
-    </div>
-  );
+  // return (
+  // <div className="h-full relative p-0">
+  //   <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+  //     <Sidebar />
+  //   </div>
+  //   <main className="md:pl-72 flex flex-col h-full">
+  //     <Navbar />
+  //     {children}
+  //     <ToastContainer
+  //       position="top-right"
+  //       autoClose={5000}
+  //       hideProgressBar={false}
+  //       newestOnTop={false}
+  //       closeOnClick
+  //       rtl={false}
+  //       pauseOnFocusLoss
+  //       draggable
+  //       pauseOnHover
+  //       theme="light"
+  //     />
+  //     {/* Same as */}
+  //     <ToastContainer />
+  //   </main>
+  // </div>
+  // );
 };
 
 export default DashboardLayout;
