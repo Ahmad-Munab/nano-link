7cb7170cb97c88b2acd32fa6a04ee0d2b17d3b81
Ahmad Munab <ahmadmunab22@gmail.com>
2024-02-29 20:07:27 +0600

    copyright date update

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,22 +1,25 @@
 const Footer = () => {
-  return (
-    <footer className="bg-white dark:bg-gray-900 mt-auto border-t bottom-0">
-      <div className="w-full mx-auto max-w-screen-xl sm:px-8 p-4 flex md:flex-row flex-col items-center md:justify-between justify-center gap-4">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://github.com/ahmad-munab" className="hover:underline">
-            Ahmad Munab
-          </a>
-          . All Rights Reserved.
-        </span>
+    return (
+        <footer className="bg-white dark:bg-gray-900 mt-auto border-t bottom-0">
+            <div className="w-full mx-auto max-w-screen-xl sm:px-8 p-4 flex md:flex-row flex-col items-center md:justify-between justify-center gap-4">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    © 2023-2036{" "}
+                    <a
+                        href="https://github.com/ahmad-munab"
+                        className="hover:underline"
+                    >
+                        Ahmad Munab
+                    </a>
+                    . All Rights Reserved.
+                </span>
 
-        <li className="flex gap-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-          Contact me:
-          <p className="hover:underline">ahmadmunab22@gmail.com</p>
-        </li>
-      </div>
-    </footer>
-  );
+                <li className="flex gap-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    Contact me:
+                    <p className="hover:underline">ahmadmunab22@gmail.com</p>
+                </li>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
